597bf4165274345a8638a27e4d9b17ed681608c1
Lior Zimmerman <lior@enzymit.com>
2023-01-10 11:01:27 +0200

    Adding the option to compute fold from embedding, rather than sequence

--- esm/esmfold/v1/esmfold.py
+++ esm/esmfold/v1/esmfold.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 
 import torch
 import torch.nn as nn
+from omegaconf import MISSING
 from openfold.data.data_transforms import make_atom14_masks
 from openfold.np import residue_constants
 from openfold.utils.loss import compute_predicted_aligned_error, compute_tm
@@ -82,14 +83,18 @@ class ESMFold(nn.Module):
     @staticmethod
     def _af2_to_esm(d: Alphabet):
         # Remember that t is shifted from residue_constants by 1 (0 is padding).
-        esm_reorder = [d.padding_idx] + [d.get_idx(v) for v in residue_constants.restypes_with_x]
+        esm_reorder = [d.padding_idx] + [
+            d.get_idx(v) for v in residue_constants.restypes_with_x
+        ]
         return torch.tensor(esm_reorder)
 
     def _af2_idx_to_esm_idx(self, aa, mask):
         aa = (aa + 1).masked_fill(mask != 1, 0)
         return self.af2_to_esm[aa]
 
-    def _compute_language_model_representations(self, esmaa: torch.Tensor) -> torch.Tensor:
+    def _compute_language_model_representations(
+        self, esmaa: torch.Tensor
+    ) -> torch.Tensor:
         """Adds bos/eos tokens for the language model, since the structure module doesn't use these."""
         batch_size = esmaa.size(0)
 
@@ -105,7 +110,9 @@ class ESMFold(nn.Module):
             repr_layers=range(self.esm.num_layers + 1),
             need_head_weights=False,
         )
-        esm_s = torch.stack([v for _, v in sorted(res["representations"].items())], dim=2)
+        esm_s = torch.stack(
+            [v for _, v in sorted(res["representations"].items())], dim=2
+        )
         esm_s = esm_s[:, 1:-1]  # B, L, nLayers, C
         return esm_s
 
@@ -117,6 +124,7 @@ class ESMFold(nn.Module):
     def forward(
         self,
         aa: torch.Tensor,
+        esm_s: T.Optional[torch.Tensor] = None,
         mask: T.Optional[torch.Tensor] = None,
         residx: T.Optional[torch.Tensor] = None,
         masking_pattern: T.Optional[torch.Tensor] = None,
@@ -153,7 +161,8 @@ class ESMFold(nn.Module):
         if masking_pattern is not None:
             esmaa = self._mask_inputs_to_esm(esmaa, masking_pattern)
 
-        esm_s = self._compute_language_model_representations(esmaa)
+        if esm_s is None:
+            esm_s = self._compute_language_model_representations(esmaa)
 
         # Convert esm_s to the precision used by the trunk and
         # the structure module. These tensors may be a lower precision if, for example,
@@ -170,7 +179,9 @@ class ESMFold(nn.Module):
 
         s_s_0 += self.embedding(aa)
 
-        structure: dict = self.trunk(s_s_0, s_z_0, aa, residx, mask, no_recycles=num_recycles)
+        structure: dict = self.trunk(
+            s_s_0, s_z_0, aa, residx, mask, no_recycles=num_recycles
+        )
         # Documenting what we expect:
         structure = {
             k: v
@@ -210,24 +221,20 @@ class ESMFold(nn.Module):
         )
         structure["lddt_head"] = lddt_head
         plddt = categorical_lddt(lddt_head[-1], bins=self.lddt_bins)
-        structure["plddt"] = (
-            100 * plddt
-        )  # we predict plDDT between 0 and 1, scale to be between 0 and 100.
+        structure["plddt"] = 100 * plddt  # we predict plDDT between 0 and 1, scale to be between 0 and 100.
 
         ptm_logits = self.ptm_head(structure["s_z"])
 
         seqlen = mask.type(torch.int64).sum(1)
         structure["ptm_logits"] = ptm_logits
-        structure["ptm"] = torch.stack(
-            [
-                compute_tm(
-                    batch_ptm_logits[None, :sl, :sl], max_bins=31, no_bins=self.distogram_bins
-                )
-                for batch_ptm_logits, sl in zip(ptm_logits, seqlen)
-            ]
-        )
+        structure["ptm"] = torch.stack([
+            compute_tm(batch_ptm_logits[None, :sl, :sl], max_bins=31, no_bins=self.distogram_bins)
+            for batch_ptm_logits, sl in zip(ptm_logits, seqlen)
+        ])
         structure.update(
-            compute_predicted_aligned_error(ptm_logits, max_bin=31, no_bins=self.distogram_bins)
+            compute_predicted_aligned_error(
+                ptm_logits, max_bin=31, no_bins=self.distogram_bins
+            )
         )
 
         return structure
@@ -241,6 +248,7 @@ class ESMFold(nn.Module):
         num_recycles: T.Optional[int] = None,
         residue_index_offset: T.Optional[int] = 512,
         chain_linker: T.Optional[str] = "G" * 25,
+        esm_s: T.Optional[torch.Tensor] = None
     ):
         """Runs a forward pass given input sequences.
 
@@ -280,9 +288,12 @@ class ESMFold(nn.Module):
             residx=residx,
             masking_pattern=masking_pattern,
             num_recycles=num_recycles,
+            esm_s=esm_s
         )
 
-        output["atom37_atom_exists"] = output["atom37_atom_exists"] * linker_mask.unsqueeze(2)
+        output["atom37_atom_exists"] = output[
+            "atom37_atom_exists"
+        ] * linker_mask.unsqueeze(2)
 
         output["mean_plddt"] = (output["plddt"] * output["atom37_atom_exists"]).sum(
             dim=(1, 2)
